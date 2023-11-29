5ef0be5a57a1028e99ef05abf94057ce3b2121b8
Oksana Tsivileva <59922105+oxanatsivileva@users.noreply.github.com>
2023-11-30 00:08:25 +0500

    Урок 3

---
+++ Lesson_3/tasks/task_05.js
@@ -0,0 +1,52 @@
+/*
+* Написать функцию, которая принимает массив чисел, например [1,2,3,4,5] и функция возращает среднее арифметическое, (округлить результат до десятых)
+* */
+
+function abs(array) {
+    let res = 0;
+    for (let i = 0; i < array.length; i++) {
+        res += array[i];
+    }
+    res =  res / array.length;
+    return Number(Math.round(res) === res ? res : res.toFixed(1))
+}
+console.log(abs([ 11, 15, 12, 123, 624, 52123, 8341234 ]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = abs
